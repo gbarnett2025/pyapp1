980d9f55d03d0b152a8a0e3e4a32822e4fafcaea
gbarnett2025 <gbarnett@cytekbio.com>
2025-12-16 08:50:21 -0800

    Initial pyapp1 using pycommon submodule

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+
+
+# Add submodule to Python path
+sys.path.append(str(Path(__file__).parent / "pycommon"))
+
+
+from appcommon import print_message
+
+
+if __name__ == "__main__":
+print_message("app1")
